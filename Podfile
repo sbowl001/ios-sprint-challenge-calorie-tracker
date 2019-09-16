6b12cd528f286e143eb87abe87c33216fe2cc645
Stephanie <sbowl001@fiu.edu>
2019-09-16 10:42:45 +0800

    reset

--- Podfile
+++
@@ -1,8 +0,0 @@
-platform :ios, '8.1'
-use_frameworks!
-
-target 'Calorie Tracker' do
-
-pod "SwiftChart"
-
-end 
--- Pods/Target Support Files/Pods-Calorie Tracker/Pods-Calorie Tracker-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Calorie_Tracker : NSObject
-@end
-@implementation PodsDummy_Pods_Calorie_Tracker
-@end
