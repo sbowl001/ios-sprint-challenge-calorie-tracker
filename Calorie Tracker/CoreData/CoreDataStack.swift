8b9c6ce959a60301b501846c88b677feaeeb358a
Stephanie <sbowl001@fiu.edu>
2019-09-16 11:48:02 +0800

    model Controller

--- Calorie Tracker/CoreData/CoreDataStack.swift
+++ Calorie Tracker/CoreData/CoreDataStack.swift
@@ -29,7 +29,7 @@ class CoreDataStack {
         return container.viewContext
     }
     
-    func save(context: NSManagedObjectContext = CoreDataStack.shared.mainContext) throws {
+    func saveToPersistentStore(context: NSManagedObjectContext = CoreDataStack.shared.mainContext) throws {
         var error: Error?
         context.performAndWait {
             do {
--- Calorie Tracker/Model/CalorieController.swift
+++ Calorie Tracker/Model/CalorieController.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+import CoreData
+
+
+class CalorieController {
+    
+    var calories: [Calorie] = []
+    
+    func addCalories(with amount: String){
+        guard let amount = Int32(amount) else {return}
+       //changing to int for coredata
+        let calorie = Calorie(calories: amount)
+        calories.append(calorie)
+        
+        do {
+            try CoreDataStack.shared.saveToPersistentStore()
+        } catch {
+            NSLog("Error saving Calorie: \(error)")
+        }
+        
+    }
+}
